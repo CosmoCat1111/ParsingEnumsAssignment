9f97fd7d79df40780f975de41f8e54b04bd08c0a
CosmoCat1111 <131853637+CosmoCat1111@users.noreply.github.com>
2024-06-18 12:58:25 -0700

    Add project files.

---
+++ ParsingEnumsAssignment/DaysOfWeek.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class DaysOfWeek
+    {
+        public enum DaysOfWeek1
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
---
+++ ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isvalid = false;
+            while (!isvalid)
+            {
+                try
+                {
+                    Console.WriteLine("Enter the current day of the week:  ");
+                    String personinput = Console.ReadLine();
+
+                    DaysOfWeek person1 = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), personinput);
+
+                    Console.WriteLine("Today is " + person1);
+                    Console.ReadLine();
+                    isvalid = true;
+
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine("Whoops, Please enter a day of the week.");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
